c59ac97b7eb964689e61cfa9477377175cb467a5
jake <palatable7@naver.com>
2022-09-25 22:15:59 +0900

    Initial Commit

---
+++ ExPaginationPhoto/ExPaginationPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExPaginationPhotoApp.swift
+//  ExPaginationPhoto
+//
+//  Created by 김종권 on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ExPaginationPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
